a3effdaa116086b8e15f3e15370c2e12cbd03d4a
髙草木 和史 <kazushi.takakusagi@gmail.com>
2025-01-01 13:46:49 +0900

    feat: backup.sh

---
+++ backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 移行先のディレクトリを指定
+BACKUP_DIR="$HOME/dev/dotfiles"
+
+# バックアップディレクトリが存在しない場合は作成
+mkdir -p "$BACKUP_DIR"
+
+# 移行するdotfileのリスト
+DOTFILES=(
+    ".bashrc"
+    ".vimrc"
+    ".gitconfig"
+    ".zshrc"
+    ".tmux.conf"
+)
+
+# 各dotfileをバックアップディレクトリにコピー
+for file in "${DOTFILES[@]}"; do
+    if [ -f "$HOME/$file" ]; then
+        cp "$HOME/$file" "$BACKUP_DIR"
+        echo "バックアップしました: $file"
+    else
+        echo "見つかりませんでした: $file"
+    fi
+done
+
+echo "dotfileの移行が完了しました。"
